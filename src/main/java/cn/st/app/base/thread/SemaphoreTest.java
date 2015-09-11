c1777f5aef215def1f43a37dad2e3468bdac2b7a
sutong <821192673@qq.com>
2015-09-11 16:29:02 +0800

    修改信号灯

--- src/main/java/cn/st/app/base/thread/SemaphoreTest.java
+++ src/main/java/cn/st/app/base/thread/SemaphoreTest.java
@@ -14,6 +14,8 @@ import java.util.concurrent.Semaphore;
  * @date 2015年9月9日
  */
 public class SemaphoreTest {
+    private static int count = 3;
+
     /**
      * 模拟10个人同时上厕所，但是只有3个坑位
      * 
@@ -21,7 +23,7 @@ public class SemaphoreTest {
      */
     public static void main(String[] args) {
         ExecutorService service = Executors.newCachedThreadPool();
-        final Semaphore sp = new Semaphore(3);
+        final Semaphore sp = new Semaphore(count);
         for (int i = 0; i < 10; i++) {
             Runnable runnable = new Runnable() {
                 public void run() {
@@ -31,13 +33,15 @@ public class SemaphoreTest {
                         e1.printStackTrace();
                     }
 
-                    System.out.println("线程" + Thread.currentThread().getName() + "进入");
+                    System.out.println("线程" + Thread.currentThread().getName() + "进入，当前"
+                            + (count - sp.availablePermits()) + "/" + count);
                     try {
                         Thread.sleep((long) (Math.random() * 10000));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    System.out.println("线程" + Thread.currentThread().getName() + "离开");
+                    System.out.println("线程" + Thread.currentThread().getName() + "离开，当前"
+                            + (count - sp.availablePermits() - 1) + "/" + count);
 
                     sp.release();
                     // 下面代码有时候执行不准确，因为其没有和上面的代码合成原子单元
